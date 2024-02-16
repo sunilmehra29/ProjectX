5c0f0de9a1db2693f9fb8cbde67f85b854ba4bce
sunilmehra29 <imsunilmehra@gmail.com>
2024-02-17 01:22:08 +0530

    testing to thik rahi

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<int:value>')
+def home(value):
+    square = value * value
+    return f"Hey the square of {value} is {square}"
+
+
+if __name__ == '__main__':
+    app.run()
